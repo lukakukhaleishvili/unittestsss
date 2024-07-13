045658030653a935c62714981b5ee25bfdf375b2
Luka Kukhaleishvili <luka.kukhaleishvili1818@gamil.com>
2024-07-13 23:57:25 +0400

    push

--- Ping/NetworkService.cs
+++ Ping/NetworkService.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -18,5 +20,55 @@ namespace NetworkUtility.Ping
         {
             return a + b;
         }
+
+
+        public DateTime LasPingDate()
+        {
+            return DateTime.Now;
+        }
+
+
+        public PingOptions GetPingOptions()
+        {
+            return new PingOptions()
+            {
+                DontFragment = true,
+                Ttl =1
+           
+            };
+        }
+
+
+        public IEnumerable<PingOptions> MostRecentPings()
+        {
+            IEnumerable<PingOptions> pingOptions = new[]
+            {
+                new PingOptions()
+                {
+                    DontFragment = true,
+                    Ttl = 1
+                },
+
+                new PingOptions()
+                {
+                    DontFragment = true,
+                    Ttl = 1
+                },
+
+                new PingOptions()
+                {
+                    DontFragment = true,
+                    Ttl = 1
+                },
+
+            };
+
+            return pingOptions;
+
+
+
+
+
+        }   
     }
 }
