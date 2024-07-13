4f82f554238f3276084448e85d3658b772f0988e
Luka Kukhaleishvili <luka.kukhaleishvili1818@gamil.com>
2024-07-13 23:26:36 +0400

    add files

---
+++ PingTests/NetworkServiceTests.cs
@@ -0,0 +1,55 @@
+﻿using FluentAssertions;
+using NetworkUtility.Ping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+////githubze taviden uech mivcet it init brdzaneba
+
+namespace NetworkUtility.Test.PingTests
+{
+    public class NetworkServiceTests
+    {
+        [Fact]
+        public void NetworkService_SendPing_ReturnString()
+        {
+            //Arrange - varieables, classess, mocks
+            var pingService = new NetworkService();
+
+            //Act
+            var result = pingService.SendPing();
+
+
+            //Assert -- fluent extension is downloaded, anristan inmemory extension
+            result.Should().NotBeNullOrWhiteSpace();
+            result.Should().Be("Success: Ping sent!");
+            result.Should().Contain("Success", Exactly.Once());
+
+        }
+
+        [Theory]
+        [InlineData(1, 2, 3)]
+        [InlineData(2, 2, 4)]
+        public void NetworkService_PingTimeout_ReturnInt(int a, int b, int expected)
+        {
+            //Arrange
+            var pingService = new NetworkService();
+
+
+            //Act
+            var result = pingService.PingTimeout(a, b);
+
+            //Assert
+            result.Should().Be(expected);
+            result.Should().BeGreaterThanOrEqualTo(2);
+            result.Should().NotBeInRange(-10000, 0);
+
+        }
+
+
+    }
+}
